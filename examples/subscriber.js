bc8cb0adcf0b8fcd3f4fa70576b8b23ab40240ed
Gene Stevens <gene@spielengine.com>
2012-08-30 00:24:48 +0000

    Example subscriber is now long-running

--- examples/subscriber.js
+++ examples/subscriber.js
@@ -2,14 +2,31 @@
 
 // access token taken from an environment variable
 var WebSocketClient = require('websocket').client,
-	accessToken = process.env.SIZE_SUBSCRIBER_TOKEN;
+	accessToken = process.env.SIZE_SUBSCRIBER_TOKEN,
+	client;
 
-var client = new WebSocketClient();
-client.on('connect', function(connection) {
-	connection.on('message', function(message) {
-		if (message.type === 'utf8') {
-			console.log("Received: '" + message.utf8Data + "'");
-		}
+function newClient() {
+	client = new WebSocketClient();
+	client.on('connect', function(connection) {
+		connection.on('error', function(error) {
+			console.log('error: '+ error.toString());
+		});
+		connection.on('close', function() {
+			client = newClient();
+		});
+		connection.on('message', function(message) {
+			if (message.type === 'utf8') {
+				if (message.utf8Data.indexOf("event") === 0)
+					console.log('Received: "' + message.utf8Data + '"');
+			}
+		});
 	});
+	client.connect('wss://api.size.io/v1.0/event/subscribe?access_token='+ accessToken);
+	return client;
+}
+
+client = newClient();
+
+process.on('uncaughtException', function(err) {
+	debug(10, 'Uncaught Exception: '+ err);
 });
-client.connect('wss://api.size.io/v1.0/event/subscribe?access_token='+ accessToken);
